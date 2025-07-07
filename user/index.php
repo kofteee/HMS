9d728497dae3192e4ce0f83d73f2eae25c0cf7da
Kerem Tufan <kerem@192.168.1.14>
2025-07-07 10:54:51 +0300

    Web pages, htmls, phps

---
+++ user/index.php
@@ -0,0 +1,13 @@
+<?php
+date_default_timezone_set('Europe/Istanbul');
+
+	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
+		$uri = 'https://';
+	} else {
+		$uri = 'http://';
+	}
+	$uri .= $_SERVER['HTTP_HOST'];
+	header('Location: '.$uri.'/dashboard/');
+	exit;
+?>
+
